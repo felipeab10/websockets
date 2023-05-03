1adfcf28c90fe49dbdf7b9f215868eb60958d1b4
Felipe Almeida <felipeab10@hotmail.com>
2023-05-03 16:49:29 -0400

    First Commit for websockest test

---
+++ api/app/Events/UserEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserEvent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('user-channel');
+    }
+
+    public function broadcastAs()
+    {
+        return 'UserEvent';
+    }
+
+    public function broadcastWith()
+    {
+        $user = User::query()->where('name', 'like', '%' . $this->message . '%')->first();
+        return ['id' => $user];
+    }
+}
